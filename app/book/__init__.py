05c7e55495beb2069ce5727d976f611fd4826237
EddieLimJiaLok <poketrainereddie@gmail.com>
2023-05-01 06:39:33 +0800

    Pre initialised the db with mock data

--- app/book/__init__.py
+++ app/book/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-book_blueprint = Blueprint('book', __name__, url_prefix='/books')
+book_blueprint = Blueprint('book', __name__, url_prefix='/api/books')
 
 from . import views
